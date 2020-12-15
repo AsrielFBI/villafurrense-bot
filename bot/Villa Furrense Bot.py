771ceb755e3b08a06e68ac1f4d9dae03c488f9af
Teko <tekofxx@gmail.com>
2020-12-15 21:51:48 +0100

    Añadida interacción cuando alguien dice yiff

--- bot/Villa Furrense Bot.py
+++ bot/Villa Furrense Bot.py
@@ -3,7 +3,8 @@ import os
 
 client = discord.Client()
 
-# When the bot stats
+
+# When the bot starts
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -16,6 +17,10 @@ async def on_message(message):
     
     if message.content.startswith('+owo'):
         await message.channel.send('OwO!')
+
+    if "yiff" in message.content:
+        await message.channel.send('¿He oído yiff?')
+        
 
 
 client.run('Nzg4NDc3MDcyOTU1NjcwNTI4.X9kEfw.yg5Q_RitwWG7K0dTlQPs4-umziQ')
