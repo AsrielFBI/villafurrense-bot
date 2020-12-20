04feafb942390d743a85876cf6968040479e6a06
Teko <tekofxx@gmail.com>
2020-12-20 16:17:19 +0100

    Coverted random to string

--- bot/main.py
+++ bot/main.py
@@ -62,7 +62,7 @@ async def on_message(message):
 
     if message.content.startswith(commandActivator+'random'):
         max=message.content.split()[-1]
-        await message.channel.send(random.randint(0, max))
+        await message.channel.send(str(random.randint(0, max)))
 
     ## FIXME
     if bot.user.id != message.author.id:
