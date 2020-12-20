6e626dcd2ee4788a63f5393ffdc15426478f1fc7
Teko <tekofxx@gmail.com>
2020-12-20 15:59:46 +0100

    Fixed help file location

--- bot/help.py
+++ bot/help.py
@@ -3,10 +3,14 @@ import os
 
 client = discord.Client()
 
+if os.path.isfile('~/proyectos/villafurrense-bot/bot'):
+    txt='~/proyectos/villafurrense-bot/bot/help.txt'
+else:
+    txt='/app/bot/help.txt'
 
 @client.event
 async def getHelp(message):
-    f = open('help.txt', 'r')
+    f = open(txt, 'r')
     general=f.read()
     await message.channel.send(general)
     f.close()
