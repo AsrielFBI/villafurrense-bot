a00ee0db4147f9fbf61b89003ff291c9b31e3c1a
Teko <tekofxx@gmail.com>
2020-12-20 15:18:46 +0100

    Completed trauma meme

--- bot/memes.py
+++ bot/memes.py
@@ -20,22 +20,22 @@ async def trauma(message):
     avatarUrl=message.mentions[0].avatar_url
     var="wget -O %s%s %s"%(memePath, "01.webp", avatarUrl)
     os.system(var)
-    convertPic(memePath+"01.webp","01",700)
+    convertPic(memePath+"01.webp","01",670)
 
     # Open images
-    background = Image.open(memePath+"trauma1.png").convert("RGBA")
+    background = Image.open(memePath+"trauma.png").convert("RGBA")
+    width, height = background.size
+    output=Image.new("RGBA",(width,height))
     img1 = Image.open(memePath+"01.png").convert("RGBA")
-    img2=Image.open(memePath+"trauma2.png").convert("RGBA")
-    background.paste(img1, (5, 400), img1)
-    background.paste(img2, (0,0), img2)
-    background.save(memePath+"output.png","PNG")
+    output.paste(img1, (39,400), img1)
+    output.paste(background, (0,0), background)
+    output.save(memePath+"output.png","PNG")
 
     # Send meme
     await message.channel.send(file=discord.File(memePath+"output.png"))
 
     # Delete user avatar and output
     os.system("rm "+memePath+"01.webp")
-    os.system("rm "+memePath+"output.png")
     os.system("rm "+memePath+"output.png")
     os.system("rm "+memePath+"01.png")
 
