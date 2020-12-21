7e802bca23416272b164ad2dae0aa9d5d18495b7
Teko <tekofxx@gmail.com>
2020-12-21 22:48:26 +0100

    Added insults

--- bot/roast.py
+++ bot/roast.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from discord.ext import commands
 
 class roast(commands.Cog):
@@ -23,6 +24,20 @@ class roast(commands.Cog):
         """Thedax puto
         """
         await context.channel.send("Thedax puto")
+
+    @commands.command(name='insult')
+    async def insult(self, context, *, user : discord.Member=None):
+        """Insulta gente 7w7
+
+            Genera un insulto aleatorio
+        """
+        insults=['eres tan fe@ que cuando enviaste tu foto por email la detectó el antivirus']
+        insults.append('eres tan fe@ que al nacer el medico dijo: Si no llora es un tumor')
+        insults.append('eres tan fe@ que cuando naciste te metieron en una incubadora con cristales tintados')
+        insults.append('tu madre era tan gorda que si se ponía un traje amarillo le gritaban taxi')
+        insults.append('eres tan torpe que cuando te caíste de la cuna, ya tenías síndrome de down')
+        output='{} %s '%(random.choice(insults))
+        await context.channel.send(output.format(user.mention))
 
 def setup(bot):
     bot.add_cog(roast(bot))
